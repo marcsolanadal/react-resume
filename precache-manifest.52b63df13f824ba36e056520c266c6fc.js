4bc370fe754f8e05b3ee69176a95581a8eb88b99
Marc Solà Nadal <marcsolanadal@gmail.com>
2019-01-03 11:48:57 +0100

    Updates

---
+++ precache-manifest.52b63df13f824ba36e056520c266c6fc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e131b120f03d213490a",
+    "url": "/react-resume/static/js/runtime~main.4e131b12.js"
+  },
+  {
+    "revision": "76f9bac72578780ba7ea",
+    "url": "/react-resume/static/js/main.76f9bac7.chunk.js"
+  },
+  {
+    "revision": "9fff8be14af71f85ec1c",
+    "url": "/react-resume/static/js/1.9fff8be1.chunk.js"
+  },
+  {
+    "revision": "6bb2433307043c12106de2c8320d35f1",
+    "url": "/react-resume/index.html"
+  }
+];
